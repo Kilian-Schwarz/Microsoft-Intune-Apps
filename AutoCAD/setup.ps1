b0e1fe71011ad480412d89e9e1611ce44202d648
Kilian-Schwarz <Kilian@webschwarz.de>
2022-04-14 02:41:40 +0200

    AutoCAD

--- AutoCAD/setup.ps1
+++ AutoCAD/setup.ps1
@@ -3,13 +3,13 @@
 Created on:   14.04.2022
 Created by:   Kilian Schwarz
 Filename:     setup.ps1
-Version:      1.1.1
-Description:  Adobe Acrobat Reader Setup über Microsoft Intune Unternehmensportal App
+Version:      1.2.1
+Description:  autodesk AutoCAD  Setup über Microsoft Intune Unternehmensportal App
 GitHub:       https://github.com/Kilian-Schwarz/Microsoft-Intune-Apps
 
 Microsoft Intune Unternehmensportal App Config:
-        Herausgeber: Adobe und Kilian Schwarz
-        Entwickler: intunewin setup.ps1 Script - Kilian Schwarz _ Adobe Acrobat Reader - Adobe
+        Herausgeber: autodesk und Kilian Schwarz
+        Entwickler: intunewin setup.ps1 Script - Kilian Schwarz _ autodesk AutoCAD  - autodesk
         Installationsbefehl: powershell.exe -executionpolicy bypass -file setup.ps1 
         Deinstallationsbefehl: powershell.exe -executionpolicy bypass -file uninstall.ps1
         Installationsverhalten: System
@@ -20,16 +20,15 @@ powershell.exe -executionpolicy bypass -file setup.ps1
 
 
 $Date = Get-Date -Format "yyyy_MM_dd_HH_mm"
-$LOG_P = "$env:TEMP\MS-Intune_LOG\Adobe-Acrobat"
-$LOG_F = "$LOG_P\Install-$Date-log_Adobe_Acrobat_Reader_Installation.log"
-$LOG_TS = "$LOG_P\Install-$Date-TS_Adobe_Acrobat_Reader_Installation.log"
+$LOG_P = "$env:TEMP\MS-Intune_LOG\autodesk-AutoCAD"
+$LOG_F = "$LOG_P\Install-$Date-log_autodesk_AutoCAD_Installation.log"
+$LOG_TS = "$LOG_P\Install-$Date-TS_autodesk_AutoCAD_Installation.log"
 
 Try
 {
     Start-Transcript -Path $LOG_TS  >> $Null
 
-    Start-Process -wait setup.exe -ArgumentList "/sAll /msi /L*v $LOG_F"
-    sleep 240
+    Start-Process -wait AutoCAD_LT_2023_German_Win_64bit_di_de-DE_setup_webinstall.exe -ArgumentList "-i install -q"
 
     Stop-Transcript  >> $Null
     return $true
---
+++ AutoCAD/uninstall.ps1
@@ -0,0 +1,5 @@
+# Funktioniert nicht korrekt
+
+MsiExec.exe /x {28B89EEF-6109-0407-2102-CF3F3A09B77D} /qn
+MsiExec.exe /x {28B89EEF-6109-0000-4102-CF3F3A09B77D} /qn
+MsiExec.exe /x {28B89EEF-6109-0407-5102-CF3F3A09B77D} /qn
